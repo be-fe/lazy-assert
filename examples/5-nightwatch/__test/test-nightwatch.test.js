0bd82282d2a63351117936c4d8363536ee4a5c49
Liang <zhengliangliang@baidu.com>
2017-08-28 15:36:09 +0800

    - wrapping page object

--- examples/5-nightwatch/__test/test-nightwatch.test.js
+++ examples/5-nightwatch/__test/test-nightwatch.test.js
@@ -1,14 +1,35 @@
 var conf = require('../nightwatch.config.js');
+var fs = require('fs');
+
+var SearchPage = function (browser) {
+    this.browser = browser;
+};
+
+SearchPage.prototype = {
+    doSearch: function (keyword) {
+        this.browser.element('xpath', '#kw')
+            .keys(keyword);
+        return this.waitFor('.result');
+    },
+    waitFor: function (xpath) {
+        return this.browser.waitForElementVisible(xpath);
+    }
+};
 
 module.exports = {
     'Demo test GitHub': function (browser) {
-        browser
-            .url('https://www.baidu.com/')   // visit the url
-            .waitForElementVisible('body'); // wait for the body to be rendered
 
-        // part two:
+        // visit the url & wait for the body to be rendered
         browser
-            .assert.containsText('body', '百度') // assert contains
+            .url('https://www.baidu.com/')
+            .waitForElementVisible('body');
+
+        var page = new SearchPage(browser);
+        page.doSearch('hello world');
+
+        // 检测 "百度百科" 文本
+        browser
+            .assert.containsText('body', 'hello world_百度百科') // assert contains
             .saveScreenshot(conf.imgpath(browser) + 'baidu.png')
             .end();
     }

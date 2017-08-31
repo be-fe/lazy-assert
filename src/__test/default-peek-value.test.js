a25a4645b26886ae7aa93ea20c23dfe44a33c61c
Liang Zheng <jspop_no1@hotmail.com>
2017-08-31 23:02:40 +0800

    - refined the test peeks
    branch at : #[ master ]#

--- src/__test/default-peek-value.test.js
+++ src/__test/default-peek-value.test.js
@@ -6,19 +6,26 @@ describe('Test default peek', function () {
     });
 
     it('Test simple value', function () {
-        lazy.peek('string-value', 'a string');
-        lazy.peek('number-value', 123);
-        lazy.peek('float-number-value', -0.123);
-        lazy.peek('boolean-value', true);
-        lazy.peek('null-value', null);
-        lazy.peek('undefined-value', undefined);
+        lazy.peek('1-simple-value', {
+            'string': 'string',
+            'number': 123,
+            'float': -0.123,
+            'NaN': Number('not-a-number'),
+            'Infinity': Infinity,
+            '-Infinity': -1 * Infinity,
+            'boolean': false,
+            'null': null,
+            'undefined': undefined,
+        });
     });
 
     it('Test complex value', function () {
-        lazy.peek('function-value', function () {
+        lazy.peek('2-complex-value', {
+            'functon': function () {},
+            'array': [1, 2, 3],
+            'object': {hello: 'world'},
+            'regexp': /abc/
         });
-        lazy.peek('array-value', [1, 2, 3]);
-        lazy.peek('object-value', {hello: 'world'});
     });
 
     it('Test complex value : deep object', function () {
@@ -34,7 +41,7 @@ describe('Test default peek', function () {
     });
 
     it('Test complex value : really deep object', function () {
-        lazy.peek('complex-deep-object', {
+        lazy.peek('3-complex-deep-object', {
             a: {b: {c: {d: {e: {f: {g: 0}}}}}},
             hello: [1, 2, 3, {
                 world: '!'
@@ -47,7 +54,7 @@ describe('Test default peek', function () {
     });
 
     it('Test complex value : really deep object, with depth = 6', function () {
-        lazy.peek('complex-deep-object-6', {
+        lazy.peek('4-complex-deep-object-6', {
             a: {b: {c: {d: {e: {f: {g: 0}}}}}},
             hello: [1, 2, 3, {
                 world: '!'
@@ -60,7 +67,7 @@ describe('Test default peek', function () {
     });
 
     it('Test complex value : really deep object, with depth = -1', function () {
-        lazy.peek('complex-deep-object-full', {
+        lazy.peek('5-complex-deep-object-full', {
             a: {b: {c: {d: {e: {f: {g: 0}}}}}},
             hello: [1, 2, 3, {
                 world: '!'
